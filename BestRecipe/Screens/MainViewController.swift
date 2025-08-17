6d5c7f7f42a25adf9daa331aca4aac7d3f93f447
Zarina Sadykova <z.sadikova@mercuryo.io>
2025-08-17 19:21:37 +0500

    Обновлен MainViewController: добавлен новый функционал

--- BestRecipe/Screens/MainViewController.swift
+++ BestRecipe/Screens/MainViewController.swift
@@ -12,7 +12,7 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .yellow
         
     }
 
