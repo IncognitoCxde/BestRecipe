a12d6a552817152767eb42f283cfac2cf00d501c
IncognitoCxde <benissaeaston@gmail.com>
2025-08-18 17:49:17 +0500

    Design System Commit
    Design sys added, Colors grouped to a folder, shortcut to font added

---
+++ BestRecipe/DesignSystem/Font.swift
@@ -0,0 +1,29 @@
+//
+//  Font.swift
+//  BestRecipe
+//
+//  Created by iMacbook on 8/18/25.
+//
+
+import UIKit
+
+struct AppFont {
+    static let Regular: String = "Poppins-Regular"
+    static let Bold: String = "Poppins-Bold"
+    static let SemiBold: String = "Poppins-SemiBold"
+    static let ExtraBold: String = "Poppins-ExtraBold"
+    static let Light: String = "Poppins-Light"
+    static let Medium: String = "Poppins-Medium"
+    static let ExtraLight: String = "Poppins-ExtraLight"
+    static let Italic: String = "Poppins-Italic"
+    static let Black: String = "Poppins-Black"
+    static let LightItalic: String = "Poppins-LightItalic"
+    static let Thin: String = "Poppins-Thin"
+    static let ExtraLightItalic: String = "Poppins-ExtraLightItalic"
+    static let ThinItalic: String = "Poppins-ThinItalic"
+    static let BlackItalic: String = "Poppins-BlackItalic"
+    static let ExtraBoldItalic: String = "Poppins-ExtraBoldItalic"
+    static let BoldItalic: String = "Poppins-BoldItalic"
+    static let SemiBoldItalic: String = "Poppins-SemiBoldItalic"
+    static let MediumItalic: String = "Poppins-MediumItalic"
+}
