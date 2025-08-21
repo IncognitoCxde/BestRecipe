2d9b20c2db84ccb2379ad2f1dc3003dd85cd018d
Administration <administration@Administrations-MacBook-Air.local>
2025-08-21 23:25:27 +0500

    feat: Add saved screen controller

---
+++ BestRecipe/Screens/SavedScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SavedScreenViewController.swift
+//  BestRecipe
+//
+//  Created by Administration  on 21/08/25.
+//
+
+import UIKit
+
+class SavedScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
