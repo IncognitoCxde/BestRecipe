817e2618f4e295a06e77153efcc103c52f67f879
Administration <administration@Administrations-MacBook-Air.local>
2025-08-27 14:00:54 +0500

    feat: implement recipe details screen with ingredients and instructions

---
+++ BestRecipe/Screens/Model/RecipeDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecipeDetailViewModel.swift
+//  BestRecipe
+//
+//  Created by Administration  on 26/08/25.
+//
+
+import Foundation
---
+++ BestRecipe/Screens/View/RecipeCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeCell.swift
+//  BestRecipe
+//
+//  Created by Administration  on 22/08/25.
+//
+
+import UIKit
+
+class RecipeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
