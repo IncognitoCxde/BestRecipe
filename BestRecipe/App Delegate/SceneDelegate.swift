70156e7c6e2f51b185b193c60910bebc849d7a47
IncognitoCxde <benissaeaston@gmail.com>
2025-08-29 20:29:17 +0500

    merge prepare commit
    ready for merge

--- BestRecipe/App Delegate/SceneDelegate.swift
+++ BestRecipe/App Delegate/SceneDelegate.swift
@@ -14,12 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        let savedRecipesVC = SavedScreenViewController()
-        let navigationController = UINavigationController(rootViewController: savedRecipesVC)
-        window.rootViewController = navigationController
-        window.makeKeyAndVisible()
-        self.window = window
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
