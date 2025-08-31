a7ee6b604085c8cf4f0ae07fac823d0ffe0306ee
IncognitoCxde <benissaeaston@gmail.com>
2025-08-31 11:10:17 +0500

    tab prep commit

--- BestRecipe/App Delegate/SceneDelegate.swift
+++ BestRecipe/App Delegate/SceneDelegate.swift
@@ -30,15 +30,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         coordinator?.start()
         print("SceneDelegate: coordinator?.start() вызван. Окно должно быть видно.")
         
-//        let helloViewModel = HelloViewModel()
-//        let helloViewController = HelloViewController(viewModel: helloViewModel)
-//        
-//        if storage.hasSeenOnboarding == true {
-//            window.rootViewController = TabBarController()
-//        } else if storage.hasSeenOnboarding == false {
-//            window.rootViewController = helloViewController
-//        }
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
