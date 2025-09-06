c71a09709a3f20b5aa87576fde9b3abdadb6fdbc
IncognitoCxde <benissaeaston@gmail.com>
2025-09-06 22:20:19 +0500

    api key commit

--- BestRecipe/Network/API/API.swift
+++ BestRecipe/Network/API/API.swift
@@ -11,11 +11,11 @@ import UIKit
 struct API {
     static let scheme = "https"
     static let host = "api.spoonacular.com"
-    static let apiKey = Token.fourth
+    static let apiKey = Token.first
 }
 
 struct Token {
-    static let first = "f566cb6be12b4681a12472763f12db1b"
+    static let first = "c351c42641664095aba2288ba0100c1e"
     static let second = "af8edef3a32c4adbac65b8c283475508"
     static let third = "651ec0da730e44c6a02c07e9abb888c3"
     static let fourth = "2415ef7c13274f62940b3053e241d579"
